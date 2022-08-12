7f9cc826343d47146821c490e5d63fc4dcad2f8b
OleksandrRabchuk <oleksandr.rabchuk@progforce.com>
2022-08-12 11:37:33 +0300

    provider

--- app.py
+++ app.py
@@ -10,15 +10,14 @@ app = flask.Flask(__name__)
 @app.route("/", methods=["POST"])
 def index():
     try:
+        os.environ["WEB3_PROVIDER_URI"] = "https://eth-mainnet.g.alchemy.com/v2/gOSHH4ng6NXaQA7zPRT1pBAQ5srcXtt0"
         data = flask.request.json
         address = data['address']
         for item, value in os.environ.items():
             print('{}: {}'.format(item, value))
-        # exp = subprocess.run(['export WEB3_PROVIDER_URI=https://eth-mainnet.g.alchemy.com/v2/gOSHH4ng6NXaQA7zPRT1pBAQ5srcXtt0'], capture_output=True, shell=True)
-        # command = f"panoramix {address}"
-        # process = subprocess.check_output(command.split()).decode('ascii').split('\n')
-        # print(exp)
-        # print(process)
+        command = f"panoramix {address}"
+        process = subprocess.check_output(command.split()).decode('ascii').split('\n')
+        print(process)
         return address
     except Exception as e:
         return flask.Response(status=400)
