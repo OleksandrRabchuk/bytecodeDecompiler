12380f794cb78af314078fcffe7a8f2bde677c62
OleksandrRabchuk <oleksandr.rabchuk@progforce.com>
2022-08-12 11:26:06 +0300

    system

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def index():
     try:
         data = flask.request.json
         address = data['address']
-        subprocess.run('set')
+        subprocess.run('platform.platform()')
         # exp = subprocess.run(['export WEB3_PROVIDER_URI=https://eth-mainnet.g.alchemy.com/v2/gOSHH4ng6NXaQA7zPRT1pBAQ5srcXtt0'], capture_output=True, shell=True)
         # command = f"panoramix {address}"
         # process = subprocess.check_output(command.split()).decode('ascii').split('\n')
