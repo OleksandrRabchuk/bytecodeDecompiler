22dd95302551b265f114dcb674a1f6ca0f7a8ba6
OleksandrRabchuk <oleksandr.rabchuk@progforce.com>
2022-08-12 11:34:39 +0300

    env

--- app.py
+++ app.py
@@ -12,13 +12,14 @@ def index():
     try:
         data = flask.request.json
         address = data['address']
-
+        for item, value in os.environ.items():
+            print('{}: {}'.format(item, value))
         # exp = subprocess.run(['export WEB3_PROVIDER_URI=https://eth-mainnet.g.alchemy.com/v2/gOSHH4ng6NXaQA7zPRT1pBAQ5srcXtt0'], capture_output=True, shell=True)
         # command = f"panoramix {address}"
         # process = subprocess.check_output(command.split()).decode('ascii').split('\n')
         # print(exp)
         # print(process)
-        return os.environ
+        return address
     except Exception as e:
         return flask.Response(status=400)
 
