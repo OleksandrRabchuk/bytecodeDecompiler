17e6ee9c87aa36bd5c2c73a88a7baf7838e71515
OleksandrRabchuk <oleksandr.rabchuk@progforce.com>
2022-08-12 11:32:47 +0300

    env

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 import flask
 import subprocess
 import json
+import os
 import platform
 app = flask.Flask(__name__)
 
@@ -11,16 +12,13 @@ def index():
     try:
         data = flask.request.json
         address = data['address']
-        info = {}
-        info['platform'] = platform.system()
-        info['platform-release'] = platform.release()
-        info['platform-version'] = platform.version()
+
         # exp = subprocess.run(['export WEB3_PROVIDER_URI=https://eth-mainnet.g.alchemy.com/v2/gOSHH4ng6NXaQA7zPRT1pBAQ5srcXtt0'], capture_output=True, shell=True)
         # command = f"panoramix {address}"
         # process = subprocess.check_output(command.split()).decode('ascii').split('\n')
         # print(exp)
         # print(process)
-        return json.dumps(info)
+        return os.environ
     except Exception as e:
         return flask.Response(status=400)
 
